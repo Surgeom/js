73e29132f8434982398183309dd915eb014d25ae
Surgeom <ushakevichkirill@gmail.com>
2021-08-03 15:52:10 +0300

    les2

--- les/script.js
+++ les/script.js
@@ -1,17 +1,62 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
-
-const renderGoodsItem = (title = 'smth', price = 0) => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-
-
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join(' ');
-    document.querySelector('.goods-list').innerHTML = goodsList
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
+function sum(arr) {
+    return arr.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+}
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    items_sum() {
+        return sum(this.goods.map(function (a) { return a.price; }))
+    }
 }
 
 
-renderGoodsList(goods);
+class Basket {
+    constructor() {
+        this.items = []
+    }
+
+    additem(item) {
+        this.items.additem(item)
+    }
+}
+
+class BasketItem {
+    remove_from_basket() {
+    }
+
+}
+
+
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+console.log(list.items_sum())
