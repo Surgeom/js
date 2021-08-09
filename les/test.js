6907cfce89edf2b51ff1723052888184580949b9
Surgeom <ushakevichkirill@gmail.com>
2021-08-09 15:43:51 +0300

    les3

--- les/test.js
+++
@@ -1,8 +0,0 @@
-goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
-
-console.log(goods.map(function (a) { return a.price; }))
